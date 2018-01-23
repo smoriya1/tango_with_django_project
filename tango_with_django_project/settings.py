62edd098f8e22e107639c5949038a35b7d065d32
Suguru <2271595m@student.gla.ac.uk>
2018-01-23 12:57:26 +0000

    Chapter 4 no exercise 2

--- tango_with_django_project/settings.py
+++ tango_with_django_project/settings.py
@@ -58,7 +58,7 @@ ROOT_URLCONF = 'tango_with_django_project.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': ['C:/tango_with_django_project/templates'],
+        'DIRS': ['H:/workspace/tango_with_django_project/templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -66,7 +66,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
-				'django.template.context_processors.media',
+		'django.template.context_processors.media',
             ],
         },
     },
