106a12cd6ac37891b5bbbcffd69dd410a02c7fb1
Suguru <2271595m@student.gla.ac.uk>
2018-02-02 13:10:01 +0000

    Chapter 7

--- rango/models.py
+++ rango/models.py
@@ -3,7 +3,8 @@ from django.template.defaultfilters import slugify
 
 # Create your models here.
 class Category(models.Model):
-    name = models.CharField(max_length=128, unique=True)
+    text_len = 128
+    name = models.CharField(max_length=text_len, unique=True)
     views = models.IntegerField(default=0)
     likes = models.IntegerField(default=0)
     slug = models.SlugField(unique=True)
@@ -20,7 +21,7 @@ class Category(models.Model):
 
 class Page(models.Model):
     category = models.ForeignKey(Category)
-    title = models.CharField(max_length=128)
+    title = models.CharField(max_length=Category.text_len)
     url = models.URLField()
     views = models.IntegerField(default=0)
 
--- rango/urls.py
+++ rango/urls.py
@@ -13,12 +13,17 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
+from django.conf import settings
+from django.conf.urls.static import static
 from django.conf.urls import url
 from rango import views
 
 urlpatterns = [
     url(r'^$',views.index, name='index'),
     url(r'^about/$',views.about, name='about'),
+    url(r'^add_category/$', views.add_category, name='add_category'),
     url(r'^category/(?P<category_name_slug>[\w\-]+)/$',
         views.show_category, name='show_category'),
-]
+     url(r'^category/(?P<category_name_slug>[\w\-]+)/add_page/$',
+        views.add_page, name='add_page'),
+]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
