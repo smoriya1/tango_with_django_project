8b62da4f86b58ede088e841a6c4d5f75e6e2bd81
Suguru <2271595m@student.gla.ac.uk>
2018-02-02 15:25:34 +0000

    Chapter 8

--- rango/urls.py
+++ rango/urls.py
@@ -17,7 +17,6 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.conf.urls import url
 from rango import views
-
 urlpatterns = [
     url(r'^$',views.index, name='index'),
     url(r'^about/$',views.about, name='about'),
--- rango/views.py
+++ rango/views.py
@@ -52,8 +52,10 @@ def index(request):
     category_list=Category.objects.order_by('-likes')[:5]
     page_list=Page.objects.order_by('-views')[:5]
     context_dict={'categories':category_list,'pages':page_list}
-    return render(request, 'rango/index.html',context_dict)
+    return render(request, 'rango/index.html', context_dict)
 
 def about(request):
-    return render(request, 'rango/about.html')
+    print(request.method)
+    print(request.user)
+    return render(request, 'rango/about.html', {})
 
